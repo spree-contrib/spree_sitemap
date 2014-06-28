86049afcb339b50f3c3da7f953221191befe5454
Michael Bianco <iloveitaly@gmail.com>
2014-06-28 10:30:46 -0400

    Adding support for spree_videos

--- lib/spree_sitemap/spree_defaults.rb
+++ lib/spree_sitemap/spree_defaults.rb
@@ -3,6 +3,7 @@ module SpreeSitemap::SpreeDefaults
     {:host => SitemapGenerator::Sitemap.default_host}
   end
   include Spree::Core::Engine.routes.url_helpers
+  include Spree::BaseHelper # for gem_available? + meta_data
 
   def add_login(options={})
     add(login_path, options)
@@ -25,7 +26,18 @@ module SpreeSitemap::SpreeDefaults
 
     add(products_path, options.merge(:lastmod => active_products.last_updated))
     active_products.each do |product|
-      add(product_path(product), options.merge(:lastmod => product.updated_at))
+      opts = options.merge(:lastmod => product.updated_at)
+
+      if gem_available?('spree_videos') && product.videos.present?
+        # TODO add exclusion list configuration option
+        # https://sites.google.com/site/webmasterhelpforum/en/faq-video-sitemaps#multiple-pages
+
+        # don't include all the videos on the page to avoid duplicate title warnings
+        primary_video = product.videos.first
+        opts.merge!(:video => [video_options(primary_video.youtube_ref, product)])
+      end
+
+      add(product_path(product), opts)
     end
   end
 
@@ -37,4 +49,28 @@ module SpreeSitemap::SpreeDefaults
     add(nested_taxons_path(taxon.permalink), options.merge(:lastmod => taxon.products.last_updated))
     taxon.children.each {|child| add_taxon(child, options) }
   end
+
+  private
+    def video_options(youtube_id, object = false)
+      # multiple videos of the same ID can exist, but all videos linked in the sitemap should be inique
+
+      # required video fields:
+      # http://www.seomoz.org/blog/video-sitemap-guide-for-vimeo-and-youtube
+
+      # youtube thumbnail images:
+      # http://www.reelseo.com/youtube-thumbnail-image/
+
+      # NOTE title should match the page title, however the title generation isn't self-contained
+      # although not a future proof solution, the best (+ easiest) solution is to mimic the title for product pages
+      #   https://github.com/spree/spree/blob/1-3-stable/core/lib/spree/core/controller_helpers/common.rb#L39
+      #   https://github.com/spree/spree/blob/1-3-stable/core/app/controllers/spree/products_controller.rb#L41
+
+      ({ :description => meta_data(object)[:description] } rescue {}).merge(
+        ({ :title => [Spree::Config[:site_name], object.name].join(' - ') } rescue {})
+      ).merge({
+        :thumbnail_loc => "http://img.youtube.com/vi/#{youtube_id}/0.jpg",
+        :player_loc => "http://www.youtube.com/v/#{youtube_id}",
+        :autoplay => "ap=1"
+      })
+    end
 end
