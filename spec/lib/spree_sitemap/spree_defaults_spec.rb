41f0eaaf90cc368a826b2ae5d2f1bc1d59684ae0
Tobias Bohwalli <hi@futhr.io>
2015-01-25 22:30:35 +0100

    Fixes spec syntax for RSpec 3.1.x.
    Move spec to correct sub folder, it still need some serious specs :/

---
+++ spec/lib/spree_sitemap/spree_defaults_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe SpreeSitemap::SpreeDefaults do
+  before do
+    @interpreter = SitemapGenerator::Interpreter.send :include, SpreeSitemap::SpreeDefaults
+  end
+
+  subject { @interpreter.new }
+
+  context 'Interpreter' do
+    %w( add_login
+        add_signup
+        add_account
+        add_password_reset
+        add_products
+        add_product
+        add_pages
+        add_taxons
+        add_taxons ).each do |method|
+      it "inherit included method #{method.to_sym}" do
+        expect(subject.respond_to?(method.to_sym)).to be(true)
+      end
+    end
+  end
+
+  context '.default_url_options' do
+    it 'returns a hash' do
+      expect(subject.default_url_options).to be_a Hash
+    end
+  end
+
+  skip '.add_login(options = {})'
+  skip '.add_signup(options = {})'
+  skip '.add_account(options = {})'
+  skip '.add_password_reset(options = {})'
+  skip '.add_products(options = {})'
+  skip '.add_product(product, options = {})'
+  skip '.add_pages(options = {})'
+  skip '.add_taxons(options = {})'
+  skip '.add_taxon(taxon, options = {})'
+end
