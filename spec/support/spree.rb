2c6542951f2a273b0e53dcf9e1a0b1fb420c8746
Tobias Bohwalli <hi@futhr.io>
2015-02-12 07:26:49 +0100

    No need for controller spec support.

--- spec/support/spree.rb
+++ spec/support/spree.rb
@@ -1,8 +1,6 @@
 require 'spree/testing_support/factories'
-require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/url_helpers'
 
 RSpec.configure do |config|
   config.include Spree::TestingSupport::UrlHelpers
-  config.include Spree::TestingSupport::ControllerRequests
 end
